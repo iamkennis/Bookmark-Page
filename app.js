d843d1169f57ec6ad2b60c60ae2792c7b9b97daa
iamkennis <kennisdamilare@gmail.com>
2020-09-03 18:32:18 +0100

    first update

---
+++ app.js
@@ -0,0 +1,31 @@
+const items = document.querySelectorAll(".accordion a");
+ 
+function toggleAccordion(){
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
+}
+ 
+items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return re.test(String(email).toLowerCase());
+}
+  
+  function validate() {
+    const $result = $("#result");
+    const email = $("#email").val();
+    $result.text("");
+  
+    if (validateEmail(email)) {
+      $result.text(email + " is valid :)");
+      $result.css("color", "green");
+    } else {
+      $result.text(email + " is not valid :(");
+      $result.css("color", "red");
+    }
+    return false;
+  }
+  
+//   $("#validate").on("click", validate);
