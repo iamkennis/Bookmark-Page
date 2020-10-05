8e816d267ca5f804c5bb016a9a11bdb027bd082e
iamkennis <kennisdamilare@gmail.com>
2020-10-05 15:03:00 +0100

    email validation

--- app.js
+++ app.js
@@ -1,31 +1,45 @@
-const items = document.querySelectorAll(".accordion a");
- 
-function toggleAccordion(){
-  this.classList.toggle('active');
-  this.nextElementSibling.classList.toggle('active');
-}
- 
-items.forEach(item => item.addEventListener('click', toggleAccordion));
 
+//TAB HEADER
 
-function validateEmail(email) {
-    const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    return re.test(String(email).toLowerCase());
-}
-  
-  function validate() {
-    const $result = $("#result");
-    const email = $("#email").val();
-    $result.text("");
-  
-    if (validateEmail(email)) {
-      $result.text(email + " is valid :)");
-      $result.css("color", "green");
-    } else {
-      $result.text(email + " is not valid :(");
-      $result.css("color", "red");
-    }
-    return false;
+function tabHeader(evt, bookName) {
+  // Declare all variables
+  let i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(bookName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+//EMAIL VALIDATION
+
+function validateForm() {
+  var email =  document.getElementById('email').value;
+  if (email == "") {
+      document.querySelector('.status').innerHTML = "Email cannot be empty";
+      return false;
+  } else {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if(!re.test(email)){
+          document.querySelector('.status').innerHTML = "Email format invalid";
+          return false;
+      }
+  }
+
   
-//   $("#validate").on("click", validate);
+ 
+}
